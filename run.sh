b483cad9e9c3f71355e855fd630489f15854e94c
drizzle9 <hexiaoyu11@jd.com>
2025-08-14 14:55:50 +0800

    add --module to compile and decompile moudle related syntax

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+rm demo.abc
+
 logdir=logs 
 ./genlogs.sh $logdir
 
-ninja -w dupbuild=warn xabc -C out 
+ninja -w dupbuild=warn xabc -C out
 LD_LIBRARY_PATH=./out/arkcompiler/runtime_core:./out/thirdparty/zlib  ./out/arkcompiler/common/xabc
 python3 scripts/removebc.py
