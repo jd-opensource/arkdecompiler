71ed845b323fffcbec5068c2ef2ee4cbfb429a94
drizzle9 <hexiaoyu11@jd.com>
2025-08-21 18:08:22 +0800

    minor

---
+++ libpandabaseheaders.h
@@ -0,0 +1,13 @@
+#include "libpandabase/mem/arena_allocator.h"
+#include "libpandabase/mem/pool_manager.h"
+
+#include "libpandafile/class_data_accessor.h"
+#include "libpandafile/class_data_accessor-inl.h"
+#include "libpandafile/method_data_accessor.h"
+
+#include "libpandafile/file.h"
+#include "libpandafile/util/collect_util.h"
+
+#include "libpandabase/mem/pool_manager.h"
+#include "libpandafile/class_data_accessor-inl.h"
+#include "libpandafile/module_data_accessor-inl.h"
--- modulevar.h
+++ modulevar.h
@@ -1,30 +1,15 @@
 #include "ast.h"
-
-#include "libpandabase/mem/arena_allocator.h"
-#include "libpandabase/mem/pool_manager.h"
-
-#include "libpandafile/class_data_accessor.h"
-#include "libpandafile/class_data_accessor-inl.h"
-#include "libpandafile/method_data_accessor.h"
-
-#include "libpandafile/file.h"
-#include "libpandafile/util/collect_util.h"
+#include "libpandabaseheaders.h"
 
 #include "assembly-parser.h"
 #include "assembler/assembly-parser.h"
-#include "compiler/optimizer/ir/basicblock.h"
-
 #include "mem/pool_manager.h"
-
 #include "disassembler/disassembler.h"
 
-#include "libpandabase/mem/pool_manager.h"
-#include "libpandafile/class_data_accessor-inl.h"
-#include "libpandafile/module_data_accessor-inl.h"
 
-using namespace std;
+
+
 using namespace panda;
-using namespace bytecodeopt;
 
 std::string GetStringByOffset(std::unique_ptr<const panda_file::File>& file_, uint32_t offset);
 
