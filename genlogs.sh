33199115490a3a9dc59f5ad6ec7e6a1170a5dc91
drizzle9 <hexiaoyu11@jd.com>
2025-08-26 16:02:19 +0800

    [feat] support module option for run.sh and genlog.sh

--- genlogs.sh
+++ genlogs.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-../out/x64.debug/arkcompiler/ets_frontend/es2abc --module --dump-assembly  demo.ts  --output demo.abc > $1/demo.pa.raw
-../out/x64.debug/arkcompiler/ets_frontend/es2abc --module --dump-ast demo.ts --output demo.abc > $1/demo.ast
-../out/x64.debug/arkcompiler/runtime_core/ark_disasm demo.abc demo.pa
+MODULE_OPTION=$2
+
+if [ "$MODULE_OPTION" = "module" ]; then
+    ../out/x64.debug/arkcompiler/ets_frontend/es2abc --module --dump-assembly  demo.ts  --output demo.abc > $1/demo.pa.raw
+    ../out/x64.debug/arkcompiler/ets_frontend/es2abc --module --dump-ast demo.ts --output demo.abc > $1/demo.ast
+    ../out/x64.debug/arkcompiler/runtime_core/ark_disasm demo.abc demo.pa
+else
+    ../out/x64.debug/arkcompiler/ets_frontend/es2abc --dump-assembly  demo.ts  --output demo.abc > $1/demo.pa.raw
+    ../out/x64.debug/arkcompiler/ets_frontend/es2abc --dump-ast demo.ts --output demo.abc > $1/demo.ast
+    ../out/x64.debug/arkcompiler/runtime_core/ark_disasm demo.abc demo.pa
+fi
