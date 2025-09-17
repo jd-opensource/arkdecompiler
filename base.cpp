79dcd2a576b8ad58c2b6f9d30b6fe9c7b7615470
drizzle9 <hexiaoyu11@jd.com>
2025-09-17 21:42:10 +0800

    feat update extractTrueFunName

--- base.cpp
+++ base.cpp
@@ -4,3 +4,26 @@ void handleError(const std::string& errorMessage) {
     std::cerr << "Error: " << errorMessage << std::endl;
     std::exit(EXIT_FAILURE); 
 }
+
+std::string extractTrueFunName(const std::string& input) {
+    std::string result;
+    size_t endPos = input.find('(');
+    if (endPos != std::string::npos) {
+        std::string beforeParen = input.substr(0, endPos);
+        size_t colonPos = beforeParen.find_last_of(':');
+        if (colonPos != std::string::npos) {
+            beforeParen = beforeParen.substr(colonPos + 1);
+        }
+
+        size_t hashPos = beforeParen.find_last_of('#');
+        std::string result;
+        if (hashPos != std::string::npos) {
+            result = beforeParen.substr(hashPos + 1);
+        } else {
+            result = beforeParen;
+        }
+        return result;
+    }else{
+        return input;
+    }
+}
