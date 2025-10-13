c3dedee6048c48c27e1b90aae1ad98ff681b390b
drizzle9 <hexiaoyu11@jd.com>
2025-10-13 16:01:06 +0800

    [tests] create tests demos

---
+++ tests/3.func.ts
@@ -0,0 +1,20 @@
+// #6. callargs callthis
+// var x1 = clearTimeout();
+// var x2 = parseInt("42");
+// var x3 = parseInt("1010", 2); 
+// var x4 = parseInt("10", 10, 0);
+// var x5 = Math.random();
+// var x6 = console.log("hello");
+
+// #9. callarg1 tLoadString
+// var obj11 = BigInt("1234567890123456789012345678901234567890");
+
+// 14. callthisrange callrange
+// var x= setInterval(Math.max, 2000, "Bob", "Hi");
+// const maxNumber = Math.max(1,2,3,4,5,6,7);
+
+// #16. function declaration and DEFINEFUNC
+// function add(a: number, b: number): number {
+//     return a + b;
+// }
+// var x = add(2, 3);
---
+++ tests/5.trycatch.ts
@@ -0,0 +1,63 @@
+// #20.1 try-catch
+// var x;
+// try {
+//   x = 1;
+// } catch (error) {
+//   x = 2;
+// }  
+// x= 5;
+
+// #20.2 try-catch with catchphi
+// var x;
+// try {
+//   x = 1;
+// } catch (error) {
+//   x = console.log(error);
+// }  
+// x= 5;
+
+// #20.3 try-catch with if-else
+// var k;
+// var x;
+// try {
+//     x = 1;
+// } catch (error1) {
+//     if(k){
+//         x = 2;
+//     }else{
+//         x = 3;
+//     }
+//     x = 4;
+// }
+// x= 5;
+
+// #20.4 try-catch
+// var x;
+// try {
+//     x = 1;
+// } catch (error) {
+//     x = 2;
+// }
+// finally{
+//   x = 3;
+// }
+// x= 4;
+
+
+// #20.5 try-catch 
+// var k;
+// var x;
+// try {
+//     x = 1;
+// } catch (error1) {
+//     if(k){
+//         x = 2;
+//     }else{
+//         x = 3;
+//     }
+//     x = 4;
+// }
+// finally{
+//     x = 5;
+// }
+// x= 6;
