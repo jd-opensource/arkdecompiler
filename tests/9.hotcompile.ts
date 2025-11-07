8940e0e9a9e0ed55437b7fabda74f52e64b0d1b5
drizzle9 <hexiaoyu11@jd.com>
2025-11-07 10:57:19 +0800

    [feat] add incremental compilation

---
+++ tests/9.hotcompile.ts
@@ -0,0 +1,26 @@
+// ./hot.sh && ./run.sh hot
+// 1. ts
+// incremental compile: base.ts
+// let a = 1;
+// let b = 1;
+// let c = 1;
+// function A() {
+//     let d = a + b;
+//     let e = 3;
+//     let m = d + e;
+//     return m;
+// }
+
+
+// 2.ts
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let n = 4;
+// function A() {
+//     let d = a + b + c;
+//     let e = 5;
+//     let m = d + e + n;
+//     return m;
+// }
+
