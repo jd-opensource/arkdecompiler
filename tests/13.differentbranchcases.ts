ba5f966dfbf530c709731e13a2eed0b0b74e3c11
drizzle9 <hexiaoyu11@jd.com>
2025-11-13 16:26:22 +0800

    [tests] minor

---
+++ tests/13.differentbranchcases.ts
@@ -0,0 +1,64 @@
+// while case 1 a
+// const list = []
+// for(let x in list) {
+//     console.log(x);
+// }
+
+// while case 1
+// var x = 0;
+// var i = 0;
+// while (i < 5) {
+//     x = i;
+//     i = i + 1;
+// }
+// x = 10;
+
+// while case 2
+// var x = 0
+// var i = 0;
+// while (true) {
+//     i++;
+//     if (i === 5) {
+//         x = i;
+//         break;
+//     }
+//     x = 1;
+// }
+// x = 3;
+
+// if case 1
+// var x;
+// var y;
+// if(x){
+//     y = console.log("true");
+//     y = console.log("1");
+// }
+
+// if case 2
+// var x;
+// var y;
+// if(x == undefined){
+//     y = console.log("1");
+// }else{
+//     y = console.log("2");
+// }
+
+// if case 3
+// var x;
+// var y;
+// if(x){
+//     y = console.log("true");
+//     y = console.log("1");
+// }else{
+//     y = console.log("false");
+// }
+
+
+// do while case 1
+// var i = 0;
+// var x = 1;
+// do {
+//     x = 1
+//     i++;
+// } while (i < 5);
+// x = 2;
