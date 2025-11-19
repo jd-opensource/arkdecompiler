8a8d494467143e2b4759ee134b244cac27083e51
drizzle9 <hexiaoyu11@jd.com>
2025-11-19 18:15:52 +0800

    [feat] add phi tests

---
+++ tests/14.phi.ts
@@ -0,0 +1,24 @@
+// #21. phi
+// function add(a, b, c) {
+//   var x;
+//   var y;
+//   if(c){
+//      x = a;
+//   }else{
+//      x = b;
+//   }
+//   y = x;
+//   return y;
+// }
+
+
+// phi in loop
+// var x = 0;
+// for (let i = 0; i < 5; i++) {
+//     if (i === 2) {
+//         x = 1;
+//         continue; 
+//     }
+//     x = 2;
+// }
+// x = 3;
