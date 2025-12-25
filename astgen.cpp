79e3bf80f3bccffad7ec3f51257a9648de8b1809
drizzle9 <hexiaoyu11@jd.com>
2025-12-25 19:13:08 +0800

    [feat] Exempt from the redundant headers in 'while' statements and the handling of 'phi' values: avoid performing repeated insertions within nested loops

--- astgen.cpp
+++ astgen.cpp
@@ -116,11 +116,15 @@ bool AstGen::RunImpl()
         }
         // check if add redundant block
         if(this->whilebody2redundant.find(bb) != this->whilebody2redundant.end()){
+            this->inserted_statements.erase(this->whilebody2redundant[bb]);
+
             this->AddInstAst2BlockStatemntByBlock(bb, this->whilebody2redundant[bb], 1);
             this->whilebody2redundant.erase(bb);
         }
     
         if(this->phiref2pendingredundant.find(bb) != this->phiref2pendingredundant.end()){
+            this->inserted_statements.erase(this->phiref2pendingredundant[bb]);
+
             this->AddInstAst2BlockStatemntByBlock(bb, this->phiref2pendingredundant[bb], 1);
             this->phiref2pendingredundant.erase(bb);
         }
