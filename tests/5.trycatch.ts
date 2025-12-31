4dce8b3b313b8117d10234f350249e4fd07b614d
drizzle9 <hexiaoyu11@jd.com>
2025-12-31 17:38:17 +0800

    [test] add a corner case for try-catch

--- tests/5.trycatch.ts
+++ tests/5.trycatch.ts
@@ -61,3 +61,9 @@
 //     x = 5;
 // }
 // x= 6;
+
+
+// corner case 1: The try basic block contains many predecessors.
+// declare const g1: Generator<number, void, string>;
+// [..._] = g1; // error
+// for (_ of g1); // error
